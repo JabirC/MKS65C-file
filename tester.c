70118d8c10b92ba98bd8d38e12bf466372a9dd5d
horazio <hfusco@stuy.edu>
2018-10-23 19:41:21 -0400

    writing into files works

--- tester.c
+++ tester.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(){
   int desc = open("rando.txt", O_WRONLY | O_WRONLY);
   char * content = malloc(100);
-  int n = 10;
-  read(desc, content, n);
-  content[0] = 1;
+  char * test_string = "this is the test string qwertyuiop";
+  int n = strlen(test_string);
+  strcpy(content, test_string);
+
   write(desc, content, n);
   int i;
   printf("\n Here are the contents of the file read into a char *\n");
 
   for(i = 0; i < 100; i++){
-    printf(" %d ", content[i]);
+    printf("%c", content[i]);
   }
   
   printf("\n\n");
