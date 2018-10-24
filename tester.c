40f312acbbd078728d40d4e469a387e4eacf7a8c
jabir chowdhury <jchowdhury@stuy.edu>
2018-10-23 22:28:38 -0400

    reads file correctly now

--- tester.c
+++ tester.c
@@ -5,20 +5,26 @@
 #include <string.h>
 
 int main(){
-  int desc = open("rando.txt", O_WRONLY | O_WRONLY);
+  int text = open("rando.txt", O_WRONLY|O_APPEND);
   char * content = malloc(100);
   char * test_string = "this is the test string qwertyuiop";
   int n = strlen(test_string);
   strcpy(content, test_string);
 
-  write(desc, content, n);
-  int i;
-  printf("\n Here are the contents written into the file:\n %s\n", test_string);
+
+  printf("\nAdding to file:\n this is the test string qwertyuiop\n");
+  write(text, content, n);
+  close(text);
+
   char * content_again = malloc(100);
-  printf("\nHere are the contents of the file read back:\n");
-  read(desc, content_again, n);
+
+
+  text = open("rando.txt", O_RDONLY);
+  printf("\nHere are the contents of the whole file read back:\n");
+  read(text, content_again, n * 3 );
   printf("%s\n\n", content_again);
 
-  close(desc);
+
+  close(text);
   return 0;
 }
